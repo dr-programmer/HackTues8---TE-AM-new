64d63630c3086c291fea0873322b9cbdf57cb4cd
1Stany1 <antong318@gmail.com>
2022-03-11 21:29:49 -1000

    funciq za id

--- sirius/lib/homepage.dart
+++ sirius/lib/homepage.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: deprecated_member_use
 
 import 'package:flutter/material.dart';
-import 'FirstQuestion.dart';
 import 'PopUp.dart';
 
 void main() => runApp(const myHomePage());
