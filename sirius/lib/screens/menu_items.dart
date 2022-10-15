412bc29f08cf538ab28bd59ce845193994715043
dr-programmer <85807185+dr-programmer@users.noreply.github.com>
2022-10-15 17:30:51 +0300

    Update profile functionality

--- sirius/lib/screens/menu_items.dart
+++ sirius/lib/screens/menu_items.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'menu_item.dart';
 
 class MenuItems {
-  static const List<MenuItem> itemsFirst = [itemSignOut];
+  static const List<MenuItem> itemsFirst = [itemUpdate, itemSignOut];
+  static const itemUpdate = MenuItem(text: ' Update', icon: Icons.update);
   static const itemSignOut = MenuItem(text: 'Sign Out', icon: Icons.logout);
 }
--- sirius/lib/screens/profile.dart
+++ sirius/lib/screens/profile.dart
@@ -6,9 +6,21 @@ import 'package:flutter_bounceable/flutter_bounceable.dart';
 import 'package:sirius/screens/signin_screen.dart';
 import 'package:sirius/screens/Variables.dart';
 import 'package:sirius/main.dart';
+import '../Question.dart';
 import 'menu_item.dart';
 import 'menu_items.dart';
 import 'about.dart';
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:path/path.dart' as path;
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:sirius/reusable_widgets/reusable_widget.dart';
+import 'package:sirius/screens/Variables.dart';
+import 'package:sirius/utils/color_utils.dart';
+import 'home_screen.dart';
 
 void demo() async {
   DocumentSnapshot documentSnapshot;
@@ -235,6 +247,10 @@ class NinjaCard extends StatelessWidget {
 
   void onSelected(BuildContext context, MenuItem item) {
     switch (item) {
+      case MenuItems.itemUpdate:
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => const Update()));
+        break;
       case MenuItems.itemSignOut:
         FirebaseAuth.instance.signOut().then((value) {
           // ignore: avoid_print
@@ -246,5 +262,236 @@ class NinjaCard extends StatelessWidget {
         });
         break;
     }
+  }
+}
+
+var _image;
+
+class Update extends StatefulWidget {
+  const Update({Key? key}) : super(key: key);
+
+  @override
+  State<Update> createState() => _Update();
+}
+
+enum ImageSourceType { gallery, camera }
+
+class _Update extends State<Update> {
+  void _handleURLButtonPress(BuildContext context, var type) {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => ImageFromGalleryEx(type)));
+  }
+
+  var Imagepicker = ImagePicker();
+
+  TextEditingController _passwordTextController = TextEditingController();
+  TextEditingController _emailTextController = TextEditingController();
+  TextEditingController _userNameTextController = TextEditingController();
+  TextEditingController _countryTextController = TextEditingController();
+  TextEditingController _cityTextController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text(
+          "Update profile info",
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+            hexStringToColor("000000"),
+            hexStringToColor("000000")
+          ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+          child: SingleChildScrollView(
+              child: Padding(
+            padding: EdgeInsets.fromLTRB(20, 120, 20, 0),
+            child: Column(
+              children: <Widget>[
+                GestureDetector(
+                  onTap: () async {
+                    XFile? image = await Imagepicker.pickImage(
+                        source: ImageSource.gallery,
+                        imageQuality: 50,
+                        preferredCameraDevice: CameraDevice.front);
+                    setState(() {
+                      _image = File(image!.path);
+                    });
+                  },
+                  child: _image != null
+                      ? CircleAvatar(
+                          radius: 80.0,
+                          backgroundImage: FileImage(_image),
+                        )
+                      : CircleAvatar(
+                          radius: 80.0,
+                          backgroundColor: Colors.red[800],
+                          child: Icon(
+                            Icons.camera_alt,
+                            color: Colors.grey[800],
+                          ),
+                        ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                reusableTextField("Username", Icons.person_outline, false,
+                    _userNameTextController),
+                const SizedBox(
+                  height: 20,
+                ),
+                reusableTextField(
+                    "E-mail", Icons.mail, false, _emailTextController),
+                const SizedBox(
+                  height: 20,
+                ),
+                reusableTextField("Password", Icons.lock_outlined, true,
+                    _passwordTextController),
+                const SizedBox(
+                  height: 20,
+                ),
+                reusableTextField("Country", Icons.location_on, false,
+                    _countryTextController),
+                const SizedBox(
+                  height: 20,
+                ),
+                reusableTextField(
+                    "City", Icons.location_city, false, _cityTextController),
+                const SizedBox(
+                  height: 20,
+                ),
+                UpdateButton(context, false, () async {
+                  email = _emailTextController.text;
+                  name = _userNameTextController.text;
+                  country = _countryTextController.text;
+                  city = _cityTextController.text;
+                  String fileName = path.basename(_image!.path);
+                  String downloadURL;
+                  var user = FirebaseAuth.instance.currentUser;
+                  user
+                      ?.updateDisplayName(_userNameTextController.toString())
+                      .then((value) {
+                    print("Profile has been changed successfully");
+                    //DO Other compilation here if you want to like setting the state of the app
+                  }).catchError((e) {
+                    print("There was an error updating profile");
+                  });
+                  user
+                      ?.updatePassword(_passwordTextController.toString())
+                      .then((value) {
+                    print("Profile has been changed successfully");
+                    //DO Other compilation here if you want to like setting the state of the app
+                  }).catchError((e) {
+                    print("There was an error updating profile");
+                  });
+                  final Reference storageReference =
+                      FirebaseStorage.instance.ref();
+                  UploadTask uploadTask =
+                      storageReference.child("image_$fileName").putFile(_image);
+                  while (await uploadTask == 0);
+                  downloadURL = await storageReference
+                      .child('image_$fileName')
+                      .getDownloadURL();
+                  FirebaseFirestore.instance
+                      .collection('Users')
+                      .doc(email)
+                      .update({
+                    'xp': xp,
+                    'name': name,
+                    'country': country,
+                    'city': city,
+                    'picture': downloadURL,
+                  });
+                  Navigator.push(context, FadeRoute(page: HomeScreen()));
+                })
+              ],
+            ),
+          ))),
+    );
+  }
+}
+
+class ImageFromGalleryEx extends StatefulWidget {
+  final type;
+  ImageFromGalleryEx(this.type);
+
+  @override
+  ImageFromGalleryExState createState() => ImageFromGalleryExState(this.type);
+}
+
+class ImageFromGalleryExState extends State<ImageFromGalleryEx> {
+  var _image;
+  var imagePicker;
+  var type;
+
+  ImageFromGalleryExState(this.type);
+
+  @override
+  void initState() {
+    super.initState();
+    imagePicker = new ImagePicker();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(type == ImageSourceType.camera
+              ? "Image from Camera"
+              : "Image from Gallery")),
+      body: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 52,
+          ),
+          Center(
+            child: GestureDetector(
+              onTap: () async {
+                var source = type == ImageSourceType.camera
+                    ? ImageSource.camera
+                    : ImageSource.gallery;
+                XFile image = await imagePicker.pickImage(
+                    source: source,
+                    imageQuality: 50,
+                    preferredCameraDevice: CameraDevice.front);
+                setState(() {
+                  _image = File(image.path);
+                });
+              },
+              child: Container(
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(color: Colors.red[200]),
+                child: _image != null
+                    ? Image.file(
+                        _image,
+                        width: 200.0,
+                        height: 200.0,
+                        fit: BoxFit.fitHeight,
+                      )
+                    : Container(
+                        decoration: BoxDecoration(
+                          color: Colors.red[200],
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        width: 200,
+                        height: 200,
+                        child: Icon(
+                          Icons.camera_alt,
+                          color: Colors.grey[800],
+                        ),
+                      ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
